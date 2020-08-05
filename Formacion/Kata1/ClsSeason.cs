0aec670e6cb76d0d1dbba178f31bb3690169a735
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-05 09:32:40 +0200

    Green: given_a_actual_date_return_his_season

---
+++ Formacion/Kata1/ClsSeason.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kata1{
+    public class ClsSeason{
+        public string GetSeason( DateTime date){
+            DateTime inicioVerano = Convert.ToDateTime("21/06/" + date.Year);
+            DateTime finVerano = Convert.ToDateTime("21/09/" + date.Year);
+
+            if((date >= inicioVerano) && (date <= finVerano)) {
+                return "Summer";
+            }
+            
+            return null;
+        }
+    }
+}
---
+++ Formacion/test/SeasonShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using FluentAssertions;
+using Kata1;
+
+namespace test{
+    public class SeasonShould{
+        [Test]
+        public void given_a_actual_date_return_his_season(){
+            var date = DateTime.Now;
+            var clsSeason = new ClsSeason();
+
+            var actualSeason = clsSeason.GetSeason(date);
+
+            actualSeason.Should().Be("Summer");
+        }
+
+
+    }
+}
