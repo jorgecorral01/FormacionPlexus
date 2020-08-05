0cd8cfe2496e4f9034998885af57a59847d69748
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-05 09:47:27 +0200

    green: given_a_twenty_one_of_March_date_return_spring

--- Formacion/Kata1/ClsSeason.cs
+++ Formacion/Kata1/ClsSeason.cs
@@ -3,13 +3,17 @@
 namespace Kata1{
     public class ClsSeason{
         public string GetSeason( DateTime date){
-            DateTime inicioVerano = Convert.ToDateTime("21/06/" + date.Year);
-            DateTime finVerano = Convert.ToDateTime("21/09/" + date.Year);
+            DateTime startSpring = Convert.ToDateTime("21/03/" + date.Year);
+            DateTime finishSpring = Convert.ToDateTime("20/06/" + date.Year);
+            DateTime startSummer = Convert.ToDateTime("21/06/" + date.Year);
+            DateTime finishSummer = Convert.ToDateTime("21/09/" + date.Year);
 
-            if((date >= inicioVerano) && (date <= finVerano)) {
+            if((date >= startSpring) && (date <= finishSpring)) {
+                return "Spring";
+            }
+            if((date >= startSummer) && (date <= finishSummer)) {
                 return "Summer";
             }
-            
             return null;
         }
     }
--- Formacion/test/SeasonShould.cs
+++ Formacion/test/SeasonShould.cs
@@ -15,6 +15,14 @@ namespace test{
             actualSeason.Should().Be("Summer");
         }
 
+        [Test]
+        public void given_a_twenty_one_of_March_date_return_spring() {
+            var date = Convert.ToDateTime("21/03/2020");
+            var clsSeason = new ClsSeason();
 
+            var actualSeason = clsSeason.GetSeason(date);
+
+            actualSeason.Should().Be("Spring");
+        }
     }
 }
