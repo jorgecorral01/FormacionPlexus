d9e9671d8625fda29d8db5a57467f5b2b208712c
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-07 09:39:47 +0200

    green: should_return_a_video_for_a_name

---
+++ Formacion/Tests/MiAPI.Actions.Test/FindVideoActionShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MiAPI.Business.Dtos;
+using MiAPI.Repositories;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MiAPI.Actions.Test {
+    public class FindVideoActionShould {
+        [Test]
+        public async Task   should_return_a_video_for_a_name(){
+            var videoRepository = new ClsVideoRepository();
+            var action = new FindVideoAction(videoRepository);
+            var nombre = "peli1";
+
+            var actualvideo = await action.Execute(nombre);
+
+            var expectVideo = new Video{name = nombre,format = "avi"};
+            actualvideo.Should().BeEquivalentTo(expectVideo);
+        }
+    }
+}
