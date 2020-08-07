d0b3a9b1b748535acccffcfec74496632e014a6f
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-07 12:51:06 +0200

    comment old test

--- Formacion/test/ClsVideoShould.cs
+++ Formacion/test/ClsVideoShould.cs
@@ -7,22 +7,22 @@ using NSubstitute;
 using NUnit.Framework;
 namespace test{
     public class ClsVideoShould{
-        [Test]
-        public void when_create_new_video_return_the_same_video(){
-            var newVideo = new Video { name = "fakeVideo", format = "avi" };
-            var clsVideoRepositoty = GivenAClsVideoRepositoty(newVideo);
-            var clsVideo = new ClsVideo(clsVideoRepositoty);
+        //[Test]
+        //public void when_create_new_video_return_the_same_video(){
+        //    var newVideo = new Video { name = "fakeVideo", format = "avi" };
+        //    var clsVideoRepositoty = GivenAClsVideoRepositoty(newVideo);
+        //    var clsVideo = new ClsVideo(clsVideoRepositoty);
 
-            var actualVideo =  clsVideo.Add(newVideo);
+        //    var actualVideo =  clsVideo.Add(newVideo);
 
-            actualVideo.Should().BeEquivalentTo(newVideo);
-            clsVideoRepositoty.Received(1).Add(newVideo);
-        }
+        //    actualVideo.Should().BeEquivalentTo(newVideo);
+        //    clsVideoRepositoty.Received(1).Add(newVideo);
+        //}
 
-        private static IClsVideoRepository GivenAClsVideoRepositoty(Video newVideo){
-            IClsVideoRepository clsVideoRepository = Substitute.For<IClsVideoRepository>();
-            clsVideoRepository.Add(newVideo).Returns(newVideo);
-            return clsVideoRepository;
-        }
+        //private static IClsVideoRepository GivenAClsVideoRepositoty(Video newVideo){
+        //    IClsVideoRepository clsVideoRepository = Substitute.For<IClsVideoRepository>();
+        //    clsVideoRepository.Add(newVideo).Returns(newVideo);
+        //    return clsVideoRepository;
+        //}
     }
 }
