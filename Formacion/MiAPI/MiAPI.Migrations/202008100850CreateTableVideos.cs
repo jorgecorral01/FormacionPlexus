010d557d96083450eb6fb7719169c7470423741c
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-10 09:41:14 +0200

    Add create table videos migration

---
+++ Formacion/MiAPI/MiAPI.Migrations/202008100850CreateTableVideos.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace MiAPI.infrastucture.SqlMigrations {
+    [Migration(202008100850)]
+    public class CreateTableVideos : Migration {
+        private const string TableName = @"Videos";
+        public override void Up() {
+            Create.Table(TableName)
+                .WithColumn("VideoId").AsInt64().NotNullable()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("Format").AsString().NotNullable()
+                .WithColumn("CreateDate").AsDateTime().NotNullable().WithDefault(SystemMethods.CurrentDateTime)
+                .WithColumn("UpdateDate").AsDateTime().NotNullable();
+        }
+
+        public override void Down() {
+            Delete.Table(TableName);
+        }
+    }
+}
