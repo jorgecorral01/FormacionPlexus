cd37a71f89a4ad01ae5b95fbd9c3ca4d046635e1
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-11 11:34:29 +0200

    create new VideoException

--- Formacion/MiAPI/MiAPI.Business/Dtos/Video.cs
+++ Formacion/MiAPI/MiAPI.Business/Dtos/Video.cs
@@ -1,8 +1,16 @@
-﻿namespace MiAPI.Business.Dtos{
+﻿using System;
+
+namespace MiAPI.Business.Dtos{
     public class Video{
         public string name{ get; set; }
         public string format{ get; set; }
     }
 
-    public class VideoNotFound : Video { }
+    public class VideoNotFoundException : Exception{
+        public readonly string Name;
+        
+        public VideoNotFoundException(string name){
+            Name = name;
+        }
+    }
 }
