94bf76930c76819211c85e99a2c2e42ee7ab77d8
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-12 13:46:16 +0200

    Swager configure and rename

--- Formacion/MiAPI/MiAPI.API/Controllers/VideoController.cs
+++ Formacion/MiAPI/MiAPI.API/Controllers/VideoController.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 using MiAPI.API.Factories;
 using MiAPI.API.swagger;
 using MiAPI.Business.Dtos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 
 namespace MiAPI.API.Controllers {
+
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class VideoController : ControllerBase{
@@ -23,16 +26,17 @@ namespace MiAPI.API.Controllers {
         public VideoController(ClsVideoRepositoryFactory clsVideoRepositoryFactory){
             _clsVideoRepositoryFactory = clsVideoRepositoryFactory;
         }
-
-        // GET api/video/name
+        
         [HttpGet("{name}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Video>> Get(string name){
             Video video =  await _clsVideoRepositoryFactory.CreateFindVideoAction().Execute(name);
              if (video is null || video is VideoNotFound) return NotFound(new Video{name = name});
              return Ok(video);
         }
 
-        // post api/video/
+        
         [HttpPost()]
         public async Task<ActionResult> Post([FromBody] Video video) {
             _clsVideoRepositoryFactory.CreateAddVideoAction().Execute(video);
@@ -41,3 +45,6 @@ namespace MiAPI.API.Controllers {
         }
     }
 }
+
+// GET api/video/name
+// post api/video/
