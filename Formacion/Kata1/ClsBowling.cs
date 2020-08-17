d6c4e398c4f9072fc6a63884400751387f603b30
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-17 13:07:07 +0200

    green: when_we_knocked_down_5_pins_on_the_first_roll_the_score_is_5 green: when_in_first_throw_we_knocked_down_10_pins_and_in_the_second_throw_5_the_score_is_20

---
+++ Formacion/Kata1/ClsBowling.cs
@@ -0,0 +1,31 @@
+﻿using System.CodeDom;
+using System.Collections.Generic;
+using System.Windows.Forms.VisualStyles;
+
+namespace Kata1{
+    public class ClsBowling{
+        private static int score;
+        private static int theThrows;
+        private static List<int> theThrowsScores;
+
+        public ClsBowling(){
+            score = 0;
+            theThrows = 0;
+            theThrowsScores = new List<int>();
+        }
+        public static void Roll(int numberPinsKnocked){
+            theThrows += 1;
+            if (theThrows % 2 == 0 && theThrowsScores[theThrows-2] == 10) {
+                score += numberPinsKnocked;
+            }
+
+            score += numberPinsKnocked;
+            theThrowsScores.Add(numberPinsKnocked);
+            
+        }
+
+        public static int Score(){
+            return score;
+        }
+    }
+}
---
+++ Formacion/test/BowlingShould.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Kata1;
+using NUnit.Framework;
+
+namespace test{
+    public class BowlingShould{
+        [Test]
+        public void when_we_knocked_down_5_pins_on_the_first_roll_the_score_is_5(){
+            var numberPinsKnocked = 5;
+            var clsBowling = new ClsBowling();
+
+            ClsBowling.Roll(numberPinsKnocked);
+
+            ClsBowling.Score().Should().Be(numberPinsKnocked);
+        }
+
+        [Test]
+        public void when_in_first_throw_we_knocked_down_10_pins_and_in_the_second_throw_5_the_score_is_20(){
+            var fisrtNumberPinsKnocked = 10;
+            var secondNumberPinsKnocked = 5;
+            var clsBowling = new ClsBowling();
+            ClsBowling.Roll(fisrtNumberPinsKnocked);
+
+            ClsBowling.Roll(secondNumberPinsKnocked);
+
+            var expectedNumberPinsKnocked = 20;
+            ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
+        }
+    }
+}
