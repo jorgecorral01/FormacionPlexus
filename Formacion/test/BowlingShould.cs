6fde305fce3099b398a224cb8ab40c2fa5f48c92
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-18 09:49:14 +0200

    when_in_ten_throw_we_have_strike_or_spare_we_can_do_another_throw

--- Formacion/test/BowlingShould.cs
+++ Formacion/test/BowlingShould.cs
@@ -15,66 +15,66 @@ namespace test{
         }
 
         [Test]
-        public void when_we_knocked_down_5_pins_on_the_first_roll_the_score_is_5(){
+        public async Task when_we_knocked_down_5_pins_on_the_first_roll_the_score_is_5(){
             var numberPinsKnocked = 5;
-            
-            ClsBowling.Roll(numberPinsKnocked);
+
+            await ClsBowling.Roll(numberPinsKnocked);
 
             ClsBowling.Score().Should().Be(numberPinsKnocked);
         }
 
         [Test]
-        public void when_in_first_throw_we_knocked_down_10_pins_and_in_the_second_throw_5_the_score_is_20(){
+        public async Task when_in_first_throw_we_knocked_down_10_pins_and_in_the_second_throw_5_the_score_is_20(){
             var fisrtNumberPinsKnocked = 10;
             var secondNumberPinsKnocked = 5;
-            ClsBowling.Roll(fisrtNumberPinsKnocked);
+            await ClsBowling.Roll(fisrtNumberPinsKnocked);
 
-            ClsBowling.Roll(secondNumberPinsKnocked);
+            await  ClsBowling.Roll(secondNumberPinsKnocked);
 
             var expectedNumberPinsKnocked = 20;
             ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
         }
 
         [Test]
-        public void when_in_first_throw_we_knocked_down_10_pins_the_next_two_throw_sum_double() {
+        public async Task when_in_first_throw_we_knocked_down_10_pins_the_next_two_throw_sum_double() {
             var fisrtNumberPinsKnocked = 10;
             var secondNumberPinsKnocked = 5;
             var thirdNumberPinsKnocked = 5;
-            ClsBowling.Roll(fisrtNumberPinsKnocked);
-            ClsBowling.Roll(secondNumberPinsKnocked);
+            await ClsBowling.Roll(fisrtNumberPinsKnocked);
+            await ClsBowling.Roll(secondNumberPinsKnocked);
 
-            ClsBowling.Roll(thirdNumberPinsKnocked);
+            await ClsBowling.Roll(thirdNumberPinsKnocked);
 
             var expectedNumberPinsKnocked = 30;
             ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
         }
 
         [Test]
-        public void when_in_second_throw_we_knocked_down_all_pins_the_next_throw_sum_double() {
+        public async Task when_in_second_throw_we_knocked_down_all_pins_the_next_throw_sum_double() {
             var fisrtNumberPinsKnocked = 1;
             var secondNumberPinsKnocked = 9;
             var thirdNumberPinsKnocked = 5;
-            ClsBowling.Roll(fisrtNumberPinsKnocked);
-            ClsBowling.Roll(secondNumberPinsKnocked);
+            await ClsBowling.Roll(fisrtNumberPinsKnocked);
+            await ClsBowling.Roll(secondNumberPinsKnocked);
 
-            ClsBowling.Roll(thirdNumberPinsKnocked);
+            await ClsBowling.Roll(thirdNumberPinsKnocked);
 
             var expectedNumberPinsKnocked = 20;
             ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
         }
 
         [Test]
-        public void when_try_do_11_throw_we_recieved_that_only_ten_times() {
-            ClsBowling.Roll(1);
-            ClsBowling.Roll(1);
-            ClsBowling.Roll(1);
-            ClsBowling.Roll(1);
-            ClsBowling.Roll(1);
-            ClsBowling.Roll(1);
-            ClsBowling.Roll(1);
-            ClsBowling.Roll(1);
-            ClsBowling.Roll(1);
-            ClsBowling.Roll(1);
+        public async Task when_try_do_11_throw_we_recieved_that_only_ten_times() {
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
 
             Func<Task> action = async () => await ClsBowling.Roll(1);
 
@@ -83,9 +83,21 @@ namespace test{
 
 
         [Test]
-        public void when_in_ten_throw_we_have_strike_or_spare_we_can_do_another_throw(){
+        public async Task when_in_ten_throw_we_have_strike_or_spare_we_can_do_another_throw(){
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(1);
+            await ClsBowling.Roll(9);
 
+            await ClsBowling.Roll(1);
 
+            ClsBowling.Score().Should().Be(20);
         }
 
         }
