69740e2a6ba22a0b61244b08b5e9962d6f0fa109
Jorge Corral <jorge.corral@domingoalonsogroup.com>
2020-08-18 12:37:44 +0200

    work in progress

--- Formacion/Kata1/ClsBowling2.cs
+++ Formacion/Kata1/ClsBowling2.cs
@@ -1,4 +1,5 @@
-﻿using System.CodeDom;
+﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Forms.VisualStyles;
@@ -6,16 +7,13 @@ using Kata1.Exceptions;
 
 namespace Kata1 {
     public class ClsBowling2 {
-        private static List<int> theThrowsScores;
+        private List<int> theThrowsScores;
 
-        public ClsBowling() {
+        public ClsBowling2() {
             theThrowsScores = new List<int>();
         }
-        public static void Roll(int numberPinsKnocked) {
-            theThrowsScores.Add(numberPinsKnocked);
-        }
-        
-        public static int Score(){
+       
+        public int Score(){
             int score = 0;
             int frameIndex = 0;
             for(int frame = 0;frame < 10;frame++) {
@@ -37,13 +35,22 @@ namespace Kata1 {
             return score;
         }
 
-        private static bool isSpare(int frameIndex) {
+        private bool isSpare(int frameIndex) {
             return theThrowsScores[frameIndex] + theThrowsScores[frameIndex + 1] == 10;
         }
 
-        private static bool isStrike(int frameIndex) {
+        private bool isStrike(int frameIndex) {
             return theThrowsScores[frameIndex] == 10;
         }
 
+        public void Roll(int[] numberPinsKnocked){
+            foreach (var tirada in numberPinsKnocked){
+                Roll(tirada);
+            }
+
+        }
+        public void Roll(int numberPinsKnocked) {
+            theThrowsScores.Add(numberPinsKnocked);
+        }
     }
 }
--- Formacion/test/BowlingShould2.cs
+++ Formacion/test/BowlingShould2.cs
@@ -7,147 +7,148 @@ using NUnit.Framework;
 
 namespace test{
     public class BowlingShould2{
-        private ClsBowling clsBowling;
+        private ClsBowling2 clsBowling2;
 
         [SetUp]
         public void Setup(){
-            clsBowling = new ClsBowling();
+            clsBowling2 = new ClsBowling2();
         }
 
         [Test]
         public void when_we_knocked_down_5_pins_on_the_first_roll_the_score_is_5(){
-            var numberPinsKnocked = 5;
+            var numberPinsKnocked = new int[10] { 5, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-            ClsBowling.Roll(numberPinsKnocked);
+            clsBowling2.Roll(numberPinsKnocked);
 
-            ClsBowling.Score().Should().Be(numberPinsKnocked);
+            var expectedScore = 5;
+            clsBowling2.Score().Should().Be(expectedScore);
         }
 
-        [Test]
-        public void when_in_first_throw_we_knocked_down_10_pins_and_in_the_second_throw_5_the_score_is_15(){
-            var fisrtNumberPinsKnocked = 10;
-            var secondNumberPinsKnocked = 5;
-             ClsBowling.Roll(fisrtNumberPinsKnocked);
+        //[Test]
+        //public void when_in_first_throw_we_knocked_down_10_pins_and_in_the_second_throw_5_the_score_is_15(){
+        //    var fisrtNumberPinsKnocked = 10;
+        //    var secondNumberPinsKnocked = 5;
+        //     ClsBowling.Roll(fisrtNumberPinsKnocked);
 
-              ClsBowling.Roll(secondNumberPinsKnocked);
+        //      ClsBowling.Roll(secondNumberPinsKnocked);
 
-            var expectedNumberPinsKnocked = 15;
-            ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
-        }
+        //    var expectedNumberPinsKnocked = 15;
+        //    ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
+        //}
 
-        [Test]
-        public void when_do_strike_the_next_two_throw_sum_double() {
-            var fisrtNumberPinsKnocked = 10;
-            var secondNumberPinsKnocked = 5;
-            var thirdNumberPinsKnocked = 5;
-            var fourthNumberPinsKnocked = 1;
-             ClsBowling.Roll(fisrtNumberPinsKnocked);
-             ClsBowling.Roll(secondNumberPinsKnocked);
+        //[Test]
+        //public void when_do_strike_the_next_two_throw_sum_double() {
+        //    var fisrtNumberPinsKnocked = 10;
+        //    var secondNumberPinsKnocked = 5;
+        //    var thirdNumberPinsKnocked = 5;
+        //    var fourthNumberPinsKnocked = 1;
+        //     ClsBowling.Roll(fisrtNumberPinsKnocked);
+        //     ClsBowling.Roll(secondNumberPinsKnocked);
 
-             ClsBowling.Roll(thirdNumberPinsKnocked);
-             ClsBowling.Roll(fourthNumberPinsKnocked);
+        //     ClsBowling.Roll(thirdNumberPinsKnocked);
+        //     ClsBowling.Roll(fourthNumberPinsKnocked);
 
-            var expectedNumberPinsKnocked = 27;
-            ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
-        }
+        //    var expectedNumberPinsKnocked = 27;
+        //    ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
+        //}
 
-        [Test]
-        public void when_do_spare_the_next_throw_sum_double() {
-            var firstNumberPinsKnocked = 1;
-            var secondNumberPinsKnocked = 9;
-            var thirdNumberPinsKnocked = 5;
-            var fourthNumberPinsKnocked = 5;
-            ClsBowling.Roll(firstNumberPinsKnocked);
-            ClsBowling.Roll(secondNumberPinsKnocked);
+        //[Test]
+        //public void when_do_spare_the_next_throw_sum_double() {
+        //    var firstNumberPinsKnocked = 1;
+        //    var secondNumberPinsKnocked = 9;
+        //    var thirdNumberPinsKnocked = 5;
+        //    var fourthNumberPinsKnocked = 5;
+        //    ClsBowling.Roll(firstNumberPinsKnocked);
+        //    ClsBowling.Roll(secondNumberPinsKnocked);
 
-            ClsBowling.Roll(thirdNumberPinsKnocked);
-            ClsBowling.Roll(fourthNumberPinsKnocked);
+        //    ClsBowling.Roll(thirdNumberPinsKnocked);
+        //    ClsBowling.Roll(fourthNumberPinsKnocked);
 
-            var expectedNumberPinsKnocked = 25;
-            ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
-        }
+        //    var expectedNumberPinsKnocked = 25;
+        //    ClsBowling.Score().Should().Be(expectedNumberPinsKnocked);
+        //}
 
-        [Test]
-        public void when_try_do_11_throw_we_recieved_that_only_ten_times() {
-             LaunchTenThrowsWithOutSpareOrStrike();
+        //[Test]
+        //public void when_try_do_11_throw_we_recieved_that_only_ten_times() {
+        //     LaunchTenThrowsWithOutSpareOrStrike();
 
-            Func<Task> action = async () =>  ClsBowling.Roll(1);
+        //    Func<Task> action = async () =>  ClsBowling.Roll(1);
 
-            action.Should().ThrowExactly<TrowsException>().Which.MessageError.Should().Be("Only ten throws");
-        }
+        //    action.Should().ThrowExactly<TrowsException>().Which.MessageError.Should().Be("Only ten throws");
+        //}
         
-        [Test]
-        public void when_in_ten_throw_we_have_strike_or_spare_we_can_do_another_throw(){
-             LaunchTenThrows();
+        //[Test]
+        //public void when_in_ten_throw_we_have_strike_or_spare_we_can_do_another_throw(){
+        //     LaunchTenThrows();
 
-             ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
 
-            ClsBowling.Score().Should().Be(20);
-        }
+        //    ClsBowling.Score().Should().Be(20);
+        //}
 
-        [Test]
-        public void when_do_the_best_game_the_score_will_be_300() {
-             LaunchTenThrowsWithStrikes();
+        //[Test]
+        //public void when_do_the_best_game_the_score_will_be_300() {
+        //     LaunchTenThrowsWithStrikes();
 
-             ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
 
-            ClsBowling.Score().Should().Be(300);
-        }
+        //    ClsBowling.Score().Should().Be(300);
+        //}
         
-        [Test]
-        public void when_do_the_best_game_unless_one_strike_the_score_will_be_270() {
-             ClsBowling.Roll(0);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
+        //[Test]
+        //public void when_do_the_best_game_unless_one_strike_the_score_will_be_270() {
+        //     ClsBowling.Roll(0);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
 
-             ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
 
-            ClsBowling.Score().Should().Be(270);
-        }
+        //    ClsBowling.Score().Should().Be(270);
+        //}
 
-        private static void LaunchTenThrows(){
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(9);
-        }
+        //private static void LaunchTenThrows(){
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(9);
+        //}
 
-        private void LaunchTenThrowsWithOutSpareOrStrike() {
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-             ClsBowling.Roll(1);
-        }
-        private void LaunchTenThrowsWithStrikes() {
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-             ClsBowling.Roll(10);
-        }
+        //private void LaunchTenThrowsWithOutSpareOrStrike() {
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //     ClsBowling.Roll(1);
+        //}
+        //private void LaunchTenThrowsWithStrikes() {
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //     ClsBowling.Roll(10);
+        //}
     }
 }
