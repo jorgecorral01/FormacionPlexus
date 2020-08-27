74b3735d5d61c78971dd46a776db68912501191a
Jorge Corral <jorge.corralcuadrado@plexus.es>
2020-08-27 08:52:23 +0200

    fix test

--- Formacion/test/ConfigureWardrobeShould.cs
+++ Formacion/test/ConfigureWardrobeShould.cs
@@ -7,31 +7,41 @@ using NUnit.Framework;
 namespace test {
     public class ConfigureWardrobeShould {
         [Test]
-        public void return_better_boxes_for_the_wall(){
+        public void return_better_boxes_for_the_wall() {
             var configureWardrobe = new ConfigureWardrobe();
 
-            var actualListBox =   configureWardrobe.GiveBetterBoxForWall();
+            var actualListBox = configureWardrobe.GiveBetterBoxForWall();
 
             var expectBox = new List<Valor>();
-            expectBox.Add(new Valor{Cajas = "50-100-100", Suma = 250});
-            expectBox.Add(new Valor { Cajas = "75-75-100", Suma = 250 });
-            expectBox.Add(new Valor { Cajas = "75-100-75", Suma = 250 });
-            expectBox.Add(new Valor { Cajas = "100-50-100", Suma = 250 }); 
-            expectBox.Add(new Valor { Cajas = "100-75-75", Suma = 250 }); 
-            expectBox.Add(new Valor { Cajas = "100-100-50", Suma = 250 });
-            expectBox.Add(new Valor { Cajas = "50-50-50-50-50", Suma = 250 });
+            expectBox.Add(new Valor { Cajas = "50-100-100", Suma = 250, Coste = 239 });
+            expectBox.Add(new Valor { Cajas = "75-75-100", Suma = 250, Coste = 214 });
+            expectBox.Add(new Valor { Cajas = "75-100-75", Suma = 250, Coste = 214 });
+            expectBox.Add(new Valor { Cajas = "100-50-100", Suma = 250, Coste = 239 });
+            expectBox.Add(new Valor { Cajas = "100-75-75", Suma = 250, Coste = 214 });
+            expectBox.Add(new Valor { Cajas = "100-100-50", Suma = 250, Coste = 239 });
+            expectBox.Add(new Valor { Cajas = "50-50-50-100", Suma = 250, Coste = 267 });
+            expectBox.Add(new Valor { Cajas = "50-50-75-75", Suma = 250, Coste = 242 });
+            expectBox.Add(new Valor { Cajas = "50-50-100-50", Suma = 250, Coste = 267 });
+            expectBox.Add(new Valor { Cajas = "50-75-50-75", Suma = 250, Coste = 242 });
+            expectBox.Add(new Valor { Cajas = "50-75-75-50", Suma = 250, Coste = 242 });
+            expectBox.Add(new Valor { Cajas = "50-100-50-50", Suma = 250, Coste = 267 });
+            expectBox.Add(new Valor { Cajas = "75-50-50-75", Suma = 250, Coste = 242 });
+            expectBox.Add(new Valor { Cajas = "75-50-75-50", Suma = 250, Coste = 242 });
+            expectBox.Add(new Valor { Cajas = "75-75-50-50", Suma = 250, Coste = 242 });
+            expectBox.Add(new Valor { Cajas = "100-50-50-50", Suma = 250, Coste = 267 });
+            expectBox.Add(new Valor { Cajas = "50-50-50-50-50", Suma = 250, Coste = 295 });
             actualListBox.Should().BeEquivalentTo(expectBox);
         }
 
         [Test]
-        public void return_better_boxes_and_cheap_for_the_wall(){
+        public void return_better_boxes_and_cheap_for_the_wall() {
             var configureWardrobe = new ConfigureWardrobe();
 
             var actualListBox = configureWardrobe.GiveBetterBoxAndCheapForWall();
 
             var expectBox = new List<Valor>();
             expectBox.Add(new Valor { Cajas = "75-75-100", Suma = 250, Coste = 214 });
-            expectBox.Add(new Valor { Cajas = "75-100-75", Suma = 250, Coste = 214 }); 
+            expectBox.Add(new Valor { Cajas = "75-100-75", Suma = 250, Coste = 214 });
             expectBox.Add(new Valor { Cajas = "100-75-75", Suma = 250, Coste = 214 });
             actualListBox.Should().BeEquivalentTo(expectBox);
         }
